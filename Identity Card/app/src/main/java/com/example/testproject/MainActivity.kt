c51b7eb4a68320950ac19d1f66dcf516f4c8e6dd
alaazamelDev <alaa.zamel23@gmail.com>
2022-10-03 21:43:11 +0300

    project: simple counter app

--- Identity Card/app/src/main/java/com/example/testproject/MainActivity.kt
+++ Identity Card/app/src/main/java/com/example/testproject/MainActivity.kt
@@ -1,26 +1,19 @@
 package com.example.testproject
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.testproject.ui.theme.TestProjectTheme
@@ -28,188 +21,67 @@ import com.example.testproject.ui.theme.TestProjectTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TestProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
-                ) {
-                    BizCard()
-                }
+                App()
             }
         }
     }
 }
 
 @Composable
-fun BizCard() {
-    val portfolioButtonState = remember {
-        mutableStateOf(value = false);
-    }
-
+fun App() {
     Surface(
-        modifier = Modifier
+        color = Color.White, modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
     ) {
-
-        // Here is the UI
-        Card(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 16.dp, vertical = 16.dp),
-            shape = RoundedCornerShape(corner = CornerSize(18.dp)),
-            backgroundColor = Color.White,
-            elevation = 4.dp,
-        ) {
-
-            Column(
-                modifier = Modifier.height(300.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                ProfileAvatar()
-                Divider(
-                    color = Color.LightGray,
-                    thickness = 1.5.dp,
-                    modifier = Modifier.padding(horizontal = 8.dp)
-                )
-                InfoSection()
-                Button(onClick = {
-                    portfolioButtonState.value = !portfolioButtonState.value
-                }) {
-                    Text(
-                        text = "Portfolio", style = MaterialTheme.typography.button
-                    )
-                }
-                if (portfolioButtonState.value) Content()
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun Content() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(5.dp)
-    ) {
-        Surface(
-            modifier = Modifier
-                .padding(3.dp)
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(
-                corner = CornerSize(6.dp)
-            ),
-            border = BorderStroke(width = 2.dp, color = Color.LightGray)
-
-        ) {
-            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
-        }
-    }
-
-}
-
-@Composable
-fun Portfolio(data: List<String>) {
-
-    LazyColumn {
-        items(data) { item: String ->
-            ProjectCard(project = item)
-        }
+        Counter()
     }
 }
 
 @Composable
-fun ProjectCard(project: String) {
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp)
-            .padding(16.dp),
-        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-        elevation = 2.dp,
-    ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ) {
-            ProfileAvatar(modifier = Modifier.size(90.dp))
-            Box(modifier = Modifier.width(8.dp))
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.Start,
-            ) {
-                Text(
-                    text = project,
-                    style = MaterialTheme.typography.h6,
-                    color = MaterialTheme.colors.primaryVariant
-                )
-                Box(modifier = Modifier.height(4.dp))
-                Text(
-                    text = "This is the second parameter",
-                    style = MaterialTheme.typography.subtitle1,
-                    color = Color.LightGray
-                )
-            }
-        }
-
+fun Counter() {
+    var count by remember {
+        mutableStateOf(0)
     }
-}
-
-@Composable
-private fun InfoSection() {
     Column(
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Alaa Zamel",
-            style = MaterialTheme.typography.h4,
-            color = MaterialTheme.colors.primaryVariant
+            text = "\$${count}",
+            style = MaterialTheme.typography.h2.copy(color = MaterialTheme.colors.primary)
         )
-        Text(
-            text = "Software Engineer", modifier = Modifier.padding(3.dp)
-        )
-        Text(
-            text = "@AlaaZamel87",
-            modifier = Modifier.padding(3.dp),
-            style = MaterialTheme.typography.subtitle1
-        )
+        Spacer(modifier = Modifier.height(128.dp))
+        CircleButton(count) {
+            count++;
+        }
     }
 }
 
 @Composable
-private fun ProfileAvatar(modifier: Modifier = Modifier) {
+private fun CircleButton(count: Int, onClicked: () -> Unit) {
+    var count1 = count
     Surface(
-        modifier = modifier
-            .size(135.dp)
-            .padding(5.dp),
-        shape = CircleShape,
-        border = BorderStroke(0.5.dp, Color.LightGray),
-        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+        modifier = Modifier
+            .size(200.dp)
+            .clickable { onClicked() },
+        color = Color.LightGray,
+        shape = CircleShape
     ) {
-
-        Image(
-            painter = painterResource(id = R.drawable.profile_image),
-            contentDescription = "profile image",
-            modifier = modifier.size(135.dp),
-            contentScale = ContentScale.Fit
-        )
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                textAlign = TextAlign.Center,
+                text = "Increment",
+                style = MaterialTheme.typography.h5
+            )
+        }
     }
 }
 
-//@Preview(showBackground = true)
+@Preview(name = "App UI", showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TestProjectTheme {
-        BizCard()
-    }
+fun DevicePreview() {
+    App()
 }
